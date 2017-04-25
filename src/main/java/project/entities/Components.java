2ebaff19d21e29fb6cbdffeb8b36443f9ec219db
AlekseyGirya <latam1984@gmail.com>
2017-04-25 18:17:34 +0300

    Second commit wit final base version 1.1

--- src/main/java/project/entities/Components.java
+++ src/main/java/project/entities/Components.java
@@ -1,9 +1,51 @@
 package project.entities;
 
 /**
- * Created by Aleksey on 25.04.2017.
+ * The class implements a set of standard methods for working
+ * with entity of the Components.
+ *
+ * @author Aleksey
  */
 public class Components implements Model {
+
+    /**
+     * The unique identifier for each component.
+     */
+    private int id;
+
+    /**
+     * The name of this component.
+     */
+    private String componentName;
+
+    /**
+     * The weight of this component.
+     */
+    private int weight;
+
+    /**
+     * The weight of this component for sale.
+     */
+    private float price;
+
+    /**
+     * The default constructor of this component for sale.
+     */
+    public Components() {
+    }
+
+    /**
+     * Constructor
+     *
+     * @param id a unique identifier fo
+     */
+    public Components(int id, String componentName, int weight, float price) {
+        this.id = id;
+        this.componentName = componentName;
+        this.weight = weight;
+        this.price = price;
+    }
+
     public void setId(long id) {
 
     }
--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS PIZZERIA
   DEFAULT CHARACTER SET utf8;
 USE PIZZERIA;
 
-# --Components of pizza with real price
+# --Components of pizza with real price.
 CREATE TABLE IF NOT EXISTS COMPONENTS (
   ID             INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
   COMPONENT_NAME VARCHAR(100) NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS COMPONENTS (
 )
   ENGINE = InnoDB;
 
-# --Stock displays all components in stock
+# --Stock displays all components in stock.
 CREATE TABLE IF NOT EXISTS STOCK (
   ID             INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
   COMPONENT_NAME VARCHAR(100) NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS STOCK (
 )
   ENGINE = InnoDB;
 
-# --Pizza contains information about pizza name and price
+# --Pizza contains information about pizza name and price.
 CREATE TABLE IF NOT EXISTS PIZZA (
   ID          INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
   PIZZA_NAME  VARCHAR(100) NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS PIZZA (
 )
   ENGINE = InnoDB;
 
-# --Complicate_pizza contains information about components of pizza
+# --Complicate_pizza contains information about components of pizza.
 CREATE TABLE IF NOT EXISTS COMPLICATE_PIZZA (
   ID           INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   ID_PIZZA     INT NOT NULL,
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS COMPLICATE_PIZZA (
 )
   ENGINE = InnoDB;
 
-# --Orders displays general information about order
+# --Orders displays general information about order.
 CREATE TABLE IF NOT EXISTS ORDERS (
   ID          INT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
   DATE        TIMESTAMP NOT NULL,
@@ -48,7 +48,7 @@ CREATE TABLE IF NOT EXISTS ORDERS (
 )
   ENGINE = InnoDB;
 
-# --Orders displays detail of orders
+# --Orders displays detail of orders.
 CREATE TABLE IF NOT EXISTS ORDERS_CONTAINS (
   ID          INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
   PIZZA_NAME  VARCHAR(100) NOT NULL,
@@ -60,7 +60,7 @@ CREATE TABLE IF NOT EXISTS ORDERS_CONTAINS (
 )
   ENGINE = InnoDB;
 
-# -- Users contains information about users and their rolls
+# -- Users contains information about users and their rolls.
 CREATE TABLE IF NOT EXISTS USERS (
   ID       INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
   ROLL     VARCHAR(50) NOT NULL,
@@ -69,7 +69,7 @@ CREATE TABLE IF NOT EXISTS USERS (
 )
   ENGINE = InnoDB;
 
-# -- History of components delivery on stock
+# -- History of components delivery on stock.
 CREATE TABLE IF NOT EXISTS STOCK_HISTORY (
   ID            INT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
   ID_COMPONENTS INT       NOT NULL,
@@ -81,7 +81,7 @@ CREATE TABLE IF NOT EXISTS STOCK_HISTORY (
   ENGINE = InnoDB;
 
 
-# -- User_order display information abot user orders
+# -- User_order display information about user orders.
 CREATE TABLE IF NOT EXISTS USER_ORDER (
   ID       INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   ID_USER  INT NOT NULL,
@@ -92,6 +92,16 @@ CREATE TABLE IF NOT EXISTS USER_ORDER (
 )
   ENGINE = InnoDB;
 
+
+# -- Stock_components displays relations stock components with components for sale and their price.
+CREATE TABLE IF NOT EXISTS STOCK_COMPONENTS(
+  ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  ID_STOCK_COMPONENT INT NOT NULL,
+  ID_COMPONENT INT NOT NULL,
+  FOREIGN KEY (ID_STOCK_COMPONENT) REFERENCES STOCK(ID),
+  FOREIGN KEY (ID_COMPONENT) REFERENCES COMPONENTS(ID)
+)
+  ENGINE = InnoDB;
 
 
 
