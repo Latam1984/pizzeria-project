88de2dcb5eab5071ad0c7274faee5c765f37eebd
AlekseyGirya <latam1984@gmail.com>
2017-05-31 22:11:02 +0300

    Change orders DAO

--- src/main/java/project/dao/OrdersDAO.java
+++ src/main/java/project/dao/OrdersDAO.java
@@ -2,6 +2,9 @@ package project.dao;
 
 import project.entities.Orders;
 
+import java.io.Serializable;
+import java.util.List;
+
 /**
  * The interface extends methods from GenericDAO for working with data base and entity
  * The interface will be implemented by DAO classes that will work with Orders entity
@@ -9,5 +12,22 @@ import project.entities.Orders;
  * @author Andrey
  */
 
-public interface OrdersDAO extends GenericDAO<Orders, Integer> {
+public interface OrdersDAO <Orders, ID> extends Serializable  {
+
+    /**
+     * Method for saving a new entity in a database
+     *
+     * @param orders an entity for saving in a database
+     * @return id of saved entity
+     */
+    ID save (Orders orders);
+
+    Orders findByID (Integer id);
+
+    ID update (Integer id);
+
+    ID delete (Integer id);
+
+    List<Orders> findAll ();
+
 }
--- src/main/java/project/dao/jdbc/JdbcOrdersDAO.java
+++ src/main/java/project/dao/jdbc/JdbcOrdersDAO.java
@@ -3,6 +3,7 @@ package project.dao.jdbc;
 import project.dao.OrdersDAO;
 import project.entities.Orders;
 
+import java.io.Serializable;
 import java.util.List;
 
 public class JdbcOrdersDAO implements OrdersDAO {
@@ -11,35 +12,31 @@ public class JdbcOrdersDAO implements OrdersDAO {
     private static final String FIND_BY_ID = "SELECT * FROM ORDERS WHERE ID = ?";
     private static final String UPDATE = "UPDATE ORDERS SET ORDER_PRICE = ? AND USER_ID = ? WHERE ID = ? ";
     private static final String DELETE = "DELETE FROM ORDERS WHERE ID = ?";
-    private static final String FIND_ALL = "SELECT * FROM ORDERS "
-
+    private static final String FIND_ALL = "SELECT * FROM ORDERS";
 
 
     @Override
-    public Integer save(Orders obj) {
+    public Object save(Object o) {
         return null;
     }
 
     @Override
-    public Orders findById(Integer integer) {
+    public Object findByID(Integer id) {
         return null;
     }
 
     @Override
-    public void update(Orders obj) {
-    }
-
-    @Override
-    public void delete(Orders obj) {
-    }
-
-    @Override
-    public List<Orders> findAll() {
+    public Object update(Integer id) {
         return null;
     }
 
     @Override
-    public Orders findByName(String name) {
+    public Object delete(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List findAll() {
         return null;
     }
 }
