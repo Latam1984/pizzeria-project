dd437e9b5025a650228b6ced71f3ac4863fc007a
andrey.demchuk <andreichdem@gmail.com>
2017-05-31 14:04:52 +0300

    cleaning rubbish

--- src/main/java/project/main/Main.java
+++ src/main/java/project/main/Main.java
@@ -1,36 +1,18 @@
 package project.main;
 
-import project.connections.ConnectionMySQL;
 
-import java.beans.Statement;
+import project.connections.ConnectionDB;
+import project.connections.ConnectionMySQL;
+import project.dao.jdbc.JdbcComponentsDAO;
+
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 /**
  * Created by Aleksey on 26.04.2017.
  */
 public class Main {
     public static void main(String[] args) {
-        ConnectionMySQL connectionMySQL = ConnectionMySQL.getConnectionMySQL();
-        Connection connection = null;
-        Statement statement = null;
 
-
-        try {
-            connection = connectionMySQL.getConnection();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        String sql;
-        sql = "SELECT * FROM USERS";
-
-        try {
-         connection.createStatement().execute(sql);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
 
 
 
